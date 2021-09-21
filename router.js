b88777ed0fb6ab099b3e8ff25b1e58cb4c344b53
James Torkornoo <torkornoojames@yahoo.co.uk>
2021-09-21 11:24:32 -0400

    Testing vercel

--- router.js
+++ router.js
@@ -2,11 +2,8 @@ const express = require("express");
 const cors = require("micro-cors");
 const router = express.Router();
 
-const handler = router.get("/", (req, res) => {
+router.get("/", (req, res) => {
   res.send("server is up and running");
-  if (req.method === "OPTIONS") {
-    return response.status(200).send("ok");
-  }
 });
 
-module.exports = cors(handler);
+module.exports = cors(router);
